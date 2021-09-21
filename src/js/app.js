56bf460e92fcb19ef0c68a828560e5a2c3d9fef8
Leonel Huachaca Velasquez <leonel.huachacav@gmail.com>
2021-09-21 18:35:40 -0500

    page finished

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,17 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    menuHamburger();
-    
+document.addEventListener("DOMContentLoaded", () => {
+  menuHamburger();
 });
 
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".nav-links");
+const main = document.querySelector(".main");
+const body = document.querySelector("body");
 
-const hamburger=document.querySelector('.hamburger');
-const menu=document.querySelector('.nav-links');
-const main=document.querySelector('.main');
-const body=document.querySelector('body');
-
-function menuHamburger(){
-
-hamburger.addEventListener('click',()=>{
-    menu.classList.toggle('active');
-    main.classList.toggle('active');
-    hamburger.classList.toggle('show');
-})
-
+function menuHamburger() {
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    main.classList.toggle("active");
+    hamburger.classList.toggle("show");
+    body.classList.toggle("no-scroll");
+  });
 }
-
-
